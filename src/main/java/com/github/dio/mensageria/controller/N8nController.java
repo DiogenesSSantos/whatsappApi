ab67a500cf067b4fda2ac6ff5288c13f0b02073a
diogenes <diogenescontatoofficial@hotmail.com>
2025-07-05 19:44:46 -0300

    Criado a mensagem padrão de resposta para api além da validação isfinalDeSemana() na classe N8NService.

--- src/main/java/com/github/dio/mensageria/controller/N8nController.java
+++ src/main/java/com/github/dio/mensageria/controller/N8nController.java
@@ -1,6 +1,8 @@
 package com.github.dio.mensageria.controller;
 
 
+import com.github.dio.mensageria.service.N8NService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,13 +18,21 @@ import java.util.Map;
 public class N8nController {
 
 
-    @PostMapping
-    public ResponseEntity<?> resposta(@RequestBody Map<String, String> corpoJson) {
+    @Autowired
+    private N8NService n8NService;
 
+
+    @PostMapping(path = {"/resposta"})
+    public ResponseEntity<?> resposta(@RequestBody Map<String, String> corpoJson) {
         System.out.println(corpoJson);
+
+        n8NService.resposta(Long.valueOf(corpoJson.get("id_cliente_bd")),
+                corpoJson.get("numero_usuario"),
+                corpoJson.get("mensagem"));
+
+
         return null;
     }
-
 
 
 }
--- src/main/java/com/github/dio/mensageria/service/N8NService.java
+++ src/main/java/com/github/dio/mensageria/service/N8NService.java
@@ -1,45 +1,117 @@
 package com.github.dio.mensageria.service;
 
 
+import com.github.dio.mensageria.model.Paciente;
+import com.github.dio.mensageria.repository.PacienteRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class N8NService {
-    private static final String URL_N8N = "https://n8n.devdiogenes.shop/webhook-test/api-java";
-    private static final Logger log = LoggerFactory.getLogger(N8NService.class);
-    private  final  RestTemplate restTemplate;
+    private static final String MSN_PADRAO_FINAL_DE_SEMANA =String.format(new StringBuilder()
+            .append("*Retirada de Comprovante de Agendamento*%n%n")
+            .append("📌 *Atenção:* esta mensagem é apenas para retirada do seu comprovante na Secretaria Municipal de Saúde de Vitória de Santo Antão.%n")
+            .append("📝 A data e o horário da sua consulta/exame só estarão disponíveis no comprovante impresso.%n%n")
+            .append("📅 *Data de retirada:* próxima segunda-feira%n")
+            .append("⏰ *Horário:* 08h00 às 14h00%n")
+            .append("📍 *Local:* Setor de Regulação%n")
+            .append("🎫 *Não esqueça:* cartão do SUS%n%n")
+            .append("✅ *Atenciosamente,  Regulação!*")
+            .toString());
 
+    private static final String MSN_PADRAO = String.format(new StringBuilder()
+            .append("*Retirada de Comprovante de Agendamento*%n%n")
+            .append("📌 *Atenção:* esta mensagem é apenas para retirada do seu comprovante na Secretaria Municipal de Saúde de Vitória de Santo Antão.%n")
+            .append("📝 A data e o horário da sua consulta/exame só estarão disponíveis no comprovante impresso.%n%n")
+            .append("📅 *Data de retirada:* Amanhã%n")
+            .append("⏰ *Horário:* 08h00 às 14h00%n")
+            .append("📍 *Local:* Setor de Regulação%n")
+            .append("🎫 *Não esqueça:* cartão do SUS%n%n")
+            .append("✅ *Atenciosamente, Regulação de Saúde!*")
+            .toString());
+
+
+    private static final String URL_N8N = "https://n8n.devdiogenes.shop/webhook/api-java";
+    private static final String URL_N8N_RESPOSTA = "https://n8n.devdiogenes.shop/webhook-test/api-java-resposta";
+    private static final Logger log = LoggerFactory.getLogger(N8NService.class);
+    private final RestTemplate restTemplate;
+
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
 
     public N8NService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    public ResponseEntity<?> enviarPayload(String idCLiente ,String nomeCliente , String numero, String mensagem) {
+
+
+
+    public ResponseEntity<?> enviarPayload(String idCLiente, String nomePaciente, String numero, String mensagem) {
         log.warn("ENVIANDO MENSAGEM PARA N8N");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
 
-        Map<String , Object> jsonCorpo = Map.of(
+        Map<String, Object> jsonCorpo = Map.of(
 
                 "id_cliente", idCLiente,
-                "nome", nomeCliente,
+                "nome", nomePaciente,
                 "telefones", numero,
                 "mensagem", mensagem
         );
 
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(jsonCorpo, headers);
 
-
         return restTemplate.exchange(
-                URL_N8N, HttpMethod.POST,request,String.class);
+                URL_N8N, HttpMethod.POST, request, String.class);
     }
 
+
+    public ResponseEntity<?> resposta(Long id, String numero, String respostaPacinete) {
+        System.out.println("RESPOSTA DO N8N");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        Map<String, Object> resposta = getResposta(numero);
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(resposta, headers);
+
+        return restTemplate.exchange(URL_N8N_RESPOSTA, HttpMethod.POST, entity, String.class);
+    }
+
+
+
+    private static Map<String, Object> getResposta(String numero) {
+        if (isFinalSemana()) {
+            Map<String, Object> resposta = Map.of(
+                    "resposta", MSN_PADRAO_FINAL_DE_SEMANA,
+                    "numero_usuario", numero);
+            return resposta;
+
+        } else {
+            Map<String, Object> resposta = Map.of(
+                    "resposta", MSN_PADRAO,
+                    "numero_usuario", numero
+            );
+
+            return resposta;
+        }
+    }
+
+
+    private static boolean isFinalSemana() {
+        LocalDate data = LocalDate.now(ZoneId.of("America/Recife")).plusDays(1L);
+        return data.getDayOfWeek().toString().equalsIgnoreCase(DayOfWeek.SATURDAY.toString()) || data.getDayOfWeek().toString().equalsIgnoreCase(DayOfWeek.SUNDAY.toString());
+    }
 
 }
